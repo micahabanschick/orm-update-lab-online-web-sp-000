4d4344bbe49d69022ea705503fc6375b90101d08
Micah Banschick <micha.ban@gmail.com>
2020-03-23 19:20:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,5 +5,29 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
-
+  attr_accessor :id, :name, :grade
+  
+  def initialize(id=nil, name, grade)
+  end 
+  
+  def self.create_table
+  end 
+  
+  def self.drop_table
+  end 
+  
+  def save 
+  end 
+  
+  def self.create(name:, grade:)
+  end 
+  
+  def self.new_from_db(row)
+  end 
+  
+  def self.find_by_name(name)
+  end 
+  
+  def update
+  end 
 end
