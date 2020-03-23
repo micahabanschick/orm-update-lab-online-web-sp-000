5a74c9f2cd63770fc0a6f068be48ba5a0df3cf30
Micah Banschick <micha.ban@gmail.com>
2020-03-23 19:29:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,12 +18,17 @@ class Student
       CREATE TABLE IF NOT EXISTS students (
       id INTEGER PRIMARY KEY,
       name TEXT,
-      grade TEXT);
-    SQL 
-    DB 
+      grade TEXT
+      );
+    SQL
+    DB[:conn].execute(sql)
   end 
   
   def self.drop_table
+    sql = <<-SQL
+      DROP TABLE IF EXISTS students;
+    SQL
+    DB[:conn].execute(sql)
   end 
   
   def save 
