81fe44524a3f13c07bb4792ba33b43fe922ff512
Micah Banschick <micha.ban@gmail.com>
2020-03-23 19:23:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,6 +8,9 @@ class Student
   attr_accessor :id, :name, :grade
   
   def initialize(id=nil, name, grade)
+    @id = id 
+    @name = name 
+    @grade = grade
   end 
   
   def self.create_table
