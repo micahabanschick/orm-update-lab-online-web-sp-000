d7fc09f7f593cf6b5a064126bee81bfa620ffe17
Micah Banschick <micha.ban@gmail.com>
2020-03-23 19:25:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,6 +14,13 @@ class Student
   end 
   
   def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade )
+    SQL 
+    sdf
   end 
   
   def self.drop_table
